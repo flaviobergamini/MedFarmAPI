ccedf6310e0678ee1811b4a0b1aad434ddf1ddaf
flaviobergamini <flaviohenrique@gec.inatel.br>
2022-07-30 01:16:48 -0300

    Migrações e arquivo gerado com o Script SQL do banco

---
+++ MedFarmAPI/Migrations/20220730041313_CreateDatabase.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedFarmAPI.Migrations
+{
+    public partial class CreateDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cpf = table.Column<string>(type: "NVARCHAR(14)", maxLength: 14, nullable: false),
+                    State = table.Column<string>(type: "NVARCHAR(45)", maxLength: 45, nullable: false),
+                    City = table.Column<string>(type: "NVARCHAR(45)", maxLength: 45, nullable: false),
+                    Complement = table.Column<string>(type: "NVARCHAR(300)", maxLength: 300, nullable: true),
+                    Cep = table.Column<string>(type: "NVARCHAR(9)", maxLength: 9, nullable: false),
+                    Street = table.Column<string>(type: "NVARCHAR(300)", maxLength: 300, nullable: false),
+                    StreetNumber = table.Column<int>(type: "INT", nullable: false),
+                    Name = table.Column<string>(type: "NVARCHAR(80)", maxLength: 80, nullable: false),
+                    Email = table.Column<string>(type: "NVARCHAR(200)", maxLength: 200, nullable: false),
+                    Phone = table.Column<string>(type: "NVARCHAR(14)", maxLength: 14, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Doctor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cpf = table.Column<string>(type: "NVARCHAR(14)", maxLength: 14, nullable: false),
+                    Specialty = table.Column<string>(type: "NVARCHAR(30)", maxLength: 30, nullable: false),
+                    RegionalCouncil = table.Column<string>(type: "NVARCHAR(30)", maxLength: 30, nullable: false),
+                    State = table.Column<string>(type: "NVARCHAR(45)", maxLength: 45, nullable: false),
+                    City = table.Column<string>(type: "NVARCHAR(45)", maxLength: 45, nullable: false),
+                    Complement = table.Column<string>(type: "NVARCHAR(300)", maxLength: 300, nullable: true),
+                    Cep = table.Column<string>(type: "NVARCHAR(9)", maxLength: 9, nullable: false),
+                    Street = table.Column<string>(type: "NVARCHAR(300)", maxLength: 300, nullable: false),
+                    StreetNumber = table.Column<int>(type: "INT", nullable: false),
+                    Name = table.Column<string>(type: "NVARCHAR(80)", maxLength: 80, nullable: false),
+                    Email = table.Column<string>(type: "NVARCHAR(200)", maxLength: 200, nullable: false),
+                    Phone = table.Column<string>(type: "NVARCHAR(14)", maxLength: 14, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doctor", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Drugstore",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cnpj = table.Column<string>(type: "NVARCHAR(14)", maxLength: 14, nullable: false),
+                    State = table.Column<string>(type: "NVARCHAR(45)", maxLength: 45, nullable: false),
+                    City = table.Column<string>(type: "NVARCHAR(45)", maxLength: 45, nullable: false),
+                    Complement = table.Column<string>(type: "NVARCHAR(300)", maxLength: 300, nullable: true),
+                    Cep = table.Column<string>(type: "NVARCHAR(9)", maxLength: 9, nullable: false),
+                    Street = table.Column<string>(type: "NVARCHAR(300)", maxLength: 300, nullable: false),
+                    StreetNumber = table.Column<int>(type: "INT", nullable: false),
+                    Name = table.Column<string>(type: "NVARCHAR(80)", maxLength: 80, nullable: false),
+                    Email = table.Column<string>(type: "NVARCHAR(200)", maxLength: 200, nullable: false),
+                    Phone = table.Column<string>(type: "NVARCHAR(14)", maxLength: 14, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drugstore", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Appointment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateTimeAppointment = table.Column<DateTime>(type: "DATETIME", nullable: false),
+                    Remote = table.Column<bool>(type: "BIT", nullable: false),
+                    VideoCallUrl = table.Column<string>(type: "TEXT", nullable: false),
+                    ClientId = table.Column<int>(type: "int", nullable: false),
+                    DoctorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Clients",
+                        column: x => x.ClientId,
+                        principalTable: "Client",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Doctor",
+                        column: x => x.DoctorId,
+                        principalTable: "Doctor",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Image = table.Column<string>(type: "TEXT", nullable: false),
+                    ClientId = table.Column<int>(type: "int", nullable: false),
+                    DrugstoresId = table.Column<int>(type: "int", nullable: false),
+                    State = table.Column<string>(type: "NVARCHAR(45)", maxLength: 45, nullable: false),
+                    City = table.Column<string>(type: "NVARCHAR(45)", maxLength: 45, nullable: false),
+                    Complement = table.Column<string>(type: "NVARCHAR(300)", maxLength: 300, nullable: true),
+                    Cep = table.Column<string>(type: "NVARCHAR(9)", maxLength: 9, nullable: false),
+                    Street = table.Column<string>(type: "NVARCHAR(300)", maxLength: 300, nullable: false),
+                    StreetNumber = table.Column<int>(type: "INT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Clients",
+                        column: x => x.ClientId,
+                        principalTable: "Client",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Orders_Drugstores",
+                        column: x => x.DrugstoresId,
+                        principalTable: "Drugstore",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointment_ClientId",
+                table: "Appointment",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointment_DoctorId",
+                table: "Appointment",
+                column: "DoctorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_ClientId",
+                table: "Order",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_DrugstoresId",
+                table: "Order",
+                column: "DrugstoresId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointment");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+
+            migrationBuilder.DropTable(
+                name: "Doctor");
+
+            migrationBuilder.DropTable(
+                name: "Client");
+
+            migrationBuilder.DropTable(
+                name: "Drugstore");
+        }
+    }
+}
---
+++ MedFarmAPI/script.sql
@@ -0,0 +1,112 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Client] (
+    [Id] int NOT NULL IDENTITY,
+    [Cpf] NVARCHAR(14) NOT NULL,
+    [State] NVARCHAR(45) NOT NULL,
+    [City] NVARCHAR(45) NOT NULL,
+    [Complement] NVARCHAR(300) NULL,
+    [Cep] NVARCHAR(9) NOT NULL,
+    [Street] NVARCHAR(300) NOT NULL,
+    [StreetNumber] INT NOT NULL,
+    [Name] NVARCHAR(80) NOT NULL,
+    [Email] NVARCHAR(200) NOT NULL,
+    [Phone] NVARCHAR(14) NOT NULL,
+    CONSTRAINT [PK_Client] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Doctor] (
+    [Id] int NOT NULL IDENTITY,
+    [Cpf] NVARCHAR(14) NOT NULL,
+    [Specialty] NVARCHAR(30) NOT NULL,
+    [RegionalCouncil] NVARCHAR(30) NOT NULL,
+    [State] NVARCHAR(45) NOT NULL,
+    [City] NVARCHAR(45) NOT NULL,
+    [Complement] NVARCHAR(300) NULL,
+    [Cep] NVARCHAR(9) NOT NULL,
+    [Street] NVARCHAR(300) NOT NULL,
+    [StreetNumber] INT NOT NULL,
+    [Name] NVARCHAR(80) NOT NULL,
+    [Email] NVARCHAR(200) NOT NULL,
+    [Phone] NVARCHAR(14) NOT NULL,
+    CONSTRAINT [PK_Doctor] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Drugstore] (
+    [Id] int NOT NULL IDENTITY,
+    [Cnpj] NVARCHAR(14) NOT NULL,
+    [State] NVARCHAR(45) NOT NULL,
+    [City] NVARCHAR(45) NOT NULL,
+    [Complement] NVARCHAR(300) NULL,
+    [Cep] NVARCHAR(9) NOT NULL,
+    [Street] NVARCHAR(300) NOT NULL,
+    [StreetNumber] INT NOT NULL,
+    [Name] NVARCHAR(80) NOT NULL,
+    [Email] NVARCHAR(200) NOT NULL,
+    [Phone] NVARCHAR(14) NOT NULL,
+    CONSTRAINT [PK_Drugstore] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Appointment] (
+    [Id] int NOT NULL IDENTITY,
+    [DateTimeAppointment] DATETIME NOT NULL,
+    [Remote] BIT NOT NULL,
+    [VideoCallUrl] TEXT NOT NULL,
+    [ClientId] int NOT NULL,
+    [DoctorId] int NOT NULL,
+    CONSTRAINT [PK_Appointment] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Appointments_Clients] FOREIGN KEY ([ClientId]) REFERENCES [Client] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Appointments_Doctor] FOREIGN KEY ([DoctorId]) REFERENCES [Doctor] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE TABLE [Order] (
+    [Id] int NOT NULL IDENTITY,
+    [Image] TEXT NOT NULL,
+    [ClientId] int NOT NULL,
+    [DrugstoresId] int NOT NULL,
+    [State] NVARCHAR(45) NOT NULL,
+    [City] NVARCHAR(45) NOT NULL,
+    [Complement] NVARCHAR(300) NULL,
+    [Cep] NVARCHAR(9) NOT NULL,
+    [Street] NVARCHAR(300) NOT NULL,
+    [StreetNumber] INT NOT NULL,
+    CONSTRAINT [PK_Order] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Orders_Clients] FOREIGN KEY ([ClientId]) REFERENCES [Client] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Orders_Drugstores] FOREIGN KEY ([DrugstoresId]) REFERENCES [Drugstore] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE INDEX [IX_Appointment_ClientId] ON [Appointment] ([ClientId]);
+GO
+
+CREATE INDEX [IX_Appointment_DoctorId] ON [Appointment] ([DoctorId]);
+GO
+
+CREATE INDEX [IX_Order_ClientId] ON [Order] ([ClientId]);
+GO
+
+CREATE INDEX [IX_Order_DrugstoresId] ON [Order] ([DrugstoresId]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20220730041313_CreateDatabase', N'6.0.7');
+GO
+
+COMMIT;
+GO
+

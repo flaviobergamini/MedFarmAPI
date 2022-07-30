a4aa7881f424ef8b24a5de12f25ce1b83fa384a2
flaviobergamini <flaviohenrique@gec.inatel.br>
2022-07-30 00:59:29 -0300

    Padronizando propriedade de id

--- MedFarmAPI/Model/UserEntity.cs
+++ MedFarmAPI/Model/UserEntity.cs
@@ -3,7 +3,7 @@ namespace MedFarmAPI.Model
 
     public abstract class UserEntity:AddressEntity
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; } = string.Empty!;
         public string Email { get; set; } = string.Empty!;
         public string Phone { get; set; } = string.Empty!;

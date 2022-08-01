50cfad2a9d2ca1e1aa297d804639e907837ae54c
flaviobergamini <flaviohenrique@gec.inatel.br>
2022-08-01 20:23:09 -0300

    Criando endpoint par verificar a disponibilidade da API

---
+++ MedFarmAPI/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MedFarmAPI.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController:ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+
+            return Ok("MedFarmAPI 2022");
+        }
+    }
+}

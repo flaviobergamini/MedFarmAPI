49c813cec9a74b18901b70d74d6d19bde81b67fb
flaviobergamini <flaviohenrique@gec.inatel.br>
2022-08-07 17:06:06 -0300

    adicionando autenticação Bearer token com JWT dentro do Swagger

--- MedFarmAPI/Program.cs
+++ MedFarmAPI/Program.cs
@@ -3,6 +3,7 @@ using MedFarmAPI.Data;
 using MedFarmAPI.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 using System.Text.Json.Serialization;
 
@@ -80,7 +81,34 @@ void ConfigureServices(WebApplicationBuilder builder)
 
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "MedFarm Api", Version = "v1" });
+
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Description =
+                "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+            Scheme = "Bearer",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.Http
+        });
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Id = "Bearer",
+                                Type = ReferenceType.SecurityScheme
+                            }
+                        },
+                        new List<string>()
+                    }
+                });
+    });
 
     // Injeção de dependencia
     builder.Services.AddDbContext<DataContext>();
